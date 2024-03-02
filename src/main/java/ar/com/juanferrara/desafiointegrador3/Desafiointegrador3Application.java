9badf88a04609fd92c7e6b8f47a285db12323e65
JuanDouCore <juanjoyqueco.8@hotmail.com>
2024-03-02 18:01:41 -0300

    first commit

---
+++ src/main/java/ar/com/juanferrara/desafiointegrador3/Desafiointegrador3Application.java
@@ -0,0 +1,13 @@
+package ar.com.juanferrara.desafiointegrador3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafiointegrador3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Desafiointegrador3Application.class, args);
+    }
+
+}
