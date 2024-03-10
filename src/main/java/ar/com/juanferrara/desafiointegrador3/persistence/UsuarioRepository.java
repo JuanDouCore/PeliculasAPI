c716de9225181a851fcf021c0f117ca3bfac1bf8
JuanDouCore <juanjoyqueco.8@hotmail.com>
2024-03-10 13:16:32 -0300

    implemented the security beans

--- src/main/java/ar/com/juanferrara/desafiointegrador3/persistence/UsuarioRepository.java
+++ src/main/java/ar/com/juanferrara/desafiointegrador3/persistence/UsuarioRepository.java
@@ -3,5 +3,8 @@ package ar.com.juanferrara.desafiointegrador3.persistence;
 import ar.com.juanferrara.desafiointegrador3.domain.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByCorreo(String correo);
 }
