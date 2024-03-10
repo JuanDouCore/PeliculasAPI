35246ced19903dd8a1305f933e21cf8bcada2361
JuanDouCore <juanjoyqueco.8@hotmail.com>
2024-03-10 14:55:48 -0300

    implemented a default admin register

--- src/main/java/ar/com/juanferrara/desafiointegrador3/Desafiointegrador3Application.java
+++ src/main/java/ar/com/juanferrara/desafiointegrador3/Desafiointegrador3Application.java
@@ -1,7 +1,14 @@
 package ar.com.juanferrara.desafiointegrador3;
 
+import ar.com.juanferrara.desafiointegrador3.domain.entity.Usuario;
+import ar.com.juanferrara.desafiointegrador3.domain.enums.Rol;
+import ar.com.juanferrara.desafiointegrador3.persistence.UsuarioRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class Desafiointegrador3Application {
@@ -11,4 +18,23 @@ public class Desafiointegrador3Application {
     }
 
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    //Generacion de admin por defecto
+    @Value("${admin.password}")
+    private String adminPassword;
+
+    @PostConstruct
+    public void initAdmin() {
+        if(!usuarioRepository.existsByCorreo("admin@admin.com")){
+            usuarioRepository.save(Usuario.builder()
+                    .correo("admin@admin.com")
+                    .contraseña(passwordEncoder.encode(adminPassword))
+                    .role(Rol.ADMIN)
+                    .build());
+        }
+    }
 }
